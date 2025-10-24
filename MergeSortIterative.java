a587bf5eef9c8461447f179dd997450e254e8afa
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 06:43:50 +0300

    Update MergeSortIterative.java

--- MergeSortIterative.java
+++ MergeSortIterative.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class MergeSortIterative {
 
+    static int comparisons = 0;
+    static int swaps = 0;
+
     public static void mergeSort(int[] arr) {
         int n = arr.length;
         int step = 1;
@@ -24,20 +27,20 @@ public class MergeSortIterative {
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
         int i = 0, j = 0, k = left;
 
         while (i < n1 && j < n2) {
+            comparisons++;
             if (L[i] <= R[j]) arr[k++] = L[i++];
             else arr[k++] = R[j++];
+            swaps++;
         }
 
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
+        while (i < n1) { arr[k++] = L[i++]; swaps++; }
+        while (j < n2) { arr[k++] = R[j++]; swaps++; }
     }
 
     public static void main(String[] args) {
@@ -45,5 +48,7 @@ public class MergeSortIterative {
         System.out.println("Початковий масив: " + Arrays.toString(arr));
         mergeSort(arr);
         System.out.println("\nВідсортований масив: " + Arrays.toString(arr));
+        System.out.println("\nКількість порівнянь: " + comparisons);
+        System.out.println("Кількість перестановок: " + swaps);
     }
 }
