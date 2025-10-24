e52b931eacaed8e8aff819017c4f8ff1dec794b2
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 06:44:30 +0300

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -2,10 +2,13 @@ import java.util.Arrays;
 
 public class QuickSort {
 
+    static int comparisons = 0;
+    static int swaps = 0;
+
     public static void quickSort(int[] arr, int low, int high, int depth) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
-            System.out.println("Глибина " + depth + " | Після розбиття (опора = " + arr[pivotIndex] + "): " + Arrays.toString(arr));
+            System.out.println("Глибина " + depth + " | Опора " + arr[pivotIndex] + ": " + Arrays.toString(arr));
             quickSort(arr, low, pivotIndex - 1, depth + 1);
             quickSort(arr, pivotIndex + 1, high, depth + 1);
         }
@@ -16,20 +19,25 @@ public class QuickSort {
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
+            comparisons++;
             if (arr[j] < pivot) {
                 i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                System.out.println("  Обмін: " + Arrays.toString(arr));
+                swap(arr, i, j);
             }
         }
 
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        System.out.println("  Перенесення опори: " + Arrays.toString(arr));
+        swap(arr, i + 1, high);
         return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            swaps++;
+            System.out.println("  Обмін: " + Arrays.toString(arr));
+        }
     }
 
     public static void main(String[] args) {
@@ -37,5 +45,7 @@ public class QuickSort {
         System.out.println("Початковий масив: " + Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1, 1);
         System.out.println("\nВідсортований масив: " + Arrays.toString(arr));
+        System.out.println("\nКількість порівнянь: " + comparisons);
+        System.out.println("Кількість перестановок: " + swaps);
     }
 }
