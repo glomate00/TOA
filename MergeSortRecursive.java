c3bb7753c5de3d730a1368309d754400e3c2e90f
glomate00 <9145326@stud.op.edu.ua>
2025-10-24 06:44:09 +0300

    Update MergeSortRecursive.java

--- MergeSortRecursive.java
+++ MergeSortRecursive.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class MergeSortRecursive {
 
+    static int comparisons = 0;
+    static int swaps = 0;
+
     public static void mergeSort(int[] arr, int left, int right, int depth) {
         if (left < right) {
             int mid = (left + right) / 2;
-            System.out.println("Глибина " + depth + ": ліва частина " + Arrays.toString(Arrays.copyOfRange(arr, left, mid + 1)));
             mergeSort(arr, left, mid, depth + 1);
-            System.out.println("Глибина " + depth + ": права частина " + Arrays.toString(Arrays.copyOfRange(arr, mid + 1, right + 1)));
             mergeSort(arr, mid + 1, right, depth + 1);
             merge(arr, left, mid, right);
             System.out.println("Злиття (" + left + "," + right + "): " + Arrays.toString(arr));
@@ -21,20 +22,20 @@ public class MergeSortRecursive {
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
         int i = 0, j = 0, k = left;
 
         while (i < n1 && j < n2) {
+            comparisons++;
             if (L[i] <= R[j]) arr[k++] = L[i++];
             else arr[k++] = R[j++];
+            swaps++;
         }
 
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
+        while (i < n1) { arr[k++] = L[i++]; swaps++; }
+        while (j < n2) { arr[k++] = R[j++]; swaps++; }
     }
 
     public static void main(String[] args) {
@@ -42,5 +43,7 @@ public class MergeSortRecursive {
         System.out.println("Початковий масив: " + Arrays.toString(arr));
         mergeSort(arr, 0, arr.length - 1, 1);
         System.out.println("\nВідсортований масив: " + Arrays.toString(arr));
+        System.out.println("\nКількість порівнянь: " + comparisons);
+        System.out.println("Кількість перестановок: " + swaps);
     }
 }
